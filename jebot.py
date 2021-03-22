13902486899cba79af985efe876ba440da4feb25
Andrew <voanerges88@gmail.com>
2021-03-22 14:37:56 +0600

    Update jebot.py

--- jebot.py
+++ jebot.py
@@ -16,8 +16,8 @@ Jebot = Client(
 
 YTDL_REGEX = (r"^((?:https?:)?\/\/)"
               r"?((?:www|m)\.)"
-              r"?((?:youtube\.com|youtu\.be|xvideos\.com|pornhub\.com"
-              r"|xhamster\.com|xnxx\.com|vk\.com|instagram\.com))"
+              r"?((?:youtube\.com|youtu\.be"
+              r"|vk\.com|instagram\.com/(?:p|tv|reel)/(?P<id>[^/?#&]+)))"
               r"(\/)([-a-zA-Z0-9()@:%_\+.~#?&//=]*)([\w\-]+)(\S+)?$")
 s2tw = OpenCC('s2tw.json').convert
 
